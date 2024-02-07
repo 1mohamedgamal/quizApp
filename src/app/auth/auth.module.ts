8591eaea7aef00b9485c33f8b1a7a6c02283dd47
1mohamedgamal <mohamedgamal.sw@gmail.com>
2024-02-07 16:50:17 +0200

    setup enviroment

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthComponent } from './auth.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { ForgetPasswordComponent } from './forget-password/forget-password.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [AuthComponent]
+  declarations: [AuthComponent, SignInComponent, SignUpComponent, ForgetPasswordComponent, ResetPasswordComponent, ChangePasswordComponent]
 })
 export class AuthModule { }
